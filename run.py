acc502e62b56b623cf53273a98af9cb3b3438c2e
Alice Chan <alicebay10@yahoo.co.uk>
2024-06-16 20:35:41 +0000

    Calculate surplus data

--- run.py
+++ run.py
@@ -66,7 +66,13 @@ def calculate_surplus_data(livessaved_row):
     print("Calculating surplus data...\n")
     vaccineproduce = SHEET.worksheet("vaccineproduce").get_all_values()
     vaccineproduce_row = vaccineproduce [-1]
-    print(vaccineproduce_row)
+    
+    surplus_data = []
+    for vaccineproduce, livessaved in zip(vaccineproduce_row, livessaved_row):
+        surplus = int(vaccineproduce) - livessaved
+        surplus_data.append(surplus)
+        
+    return surplus_data
     
 
 
@@ -77,7 +83,8 @@ def main():
     data = get_livessaved_data()
     livessaved_data = [int(num) for num in data]
     update_livessaved_worksheet(livessaved_data)
-    calculate_surplus_data(livessaved_data)
+    new_surplus_data = calculate_surplus_data(livessaved_data)
+    print(new_surplus_data)
     
 
 print("Welcome to Vaccines Africa Data Automation")
